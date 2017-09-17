d12fc52999edea3cf97f4f39e6d008bfc51f975b
yans67 <1018743361@qq.com>
2017-09-17 12:16:42 +0800

    添加时钟壁纸

--- Clock/Clock/clock/wallpaper/WallpaperController.m
+++ Clock/Clock/clock/wallpaper/WallpaperController.m
@@ -18,36 +18,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
     [self updateTime];
     
     //定时器 反复执行
     NSTimer *timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(updateTime) userInfo:nil repeats:YES];
     [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
     
+    // 添加点击手势
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(dismissController)];
     [self.view addGestureRecognizer:tap];
-    
-    
+
     // 设置屏幕常亮，默认是NO。亮度范围是0.1 - 1.0
     [UIApplication sharedApplication].idleTimerDisabled = YES;
 //    [[UIScreen mainScreen] setBrightness: 0.5];
     
 }
 
-- (void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:animated];
-    // 获取屏幕亮度
-    self.brightness = [UIScreen mainScreen].brightness;
-}
-
-- (void)viewDidDisappear:(BOOL)animated{
-    [super viewDidDisappear:animated];
-    // 恢复屏幕亮度
-    [UIScreen mainScreen].brightness = self.brightness;
-}
-
+#pragma mark - 更新时间
 - (void)updateTime{
     NSDate *currentDate = [NSDate date];
     NSDateFormatter *dataFormatter = [[NSDateFormatter alloc]init];
@@ -56,6 +43,7 @@
     self.timeLab.text = dateString;
 }
 
+#pragma mark - 点击手势事件，控制器消失
 - (void)dismissController{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
@@ -65,6 +53,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - 获取屏幕亮度
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.brightness = [UIScreen mainScreen].brightness;
+}
+
+#pragma mark - 恢复屏幕亮度
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [UIScreen mainScreen].brightness = self.brightness;
+}
 
 
 /*
